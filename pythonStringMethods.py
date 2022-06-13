2d710fa4f483a78f182f04b388bba88822413a8c
Abhishek <abhshekyadav2k16@gmailcom>
2022-06-13 12:18:39 +0530

    Python String Methods

---
+++ pythonStringMethods.py
@@ -0,0 +1,51 @@
+ # 1 capitailze() This the methods convert the first Character of a string
+ 
+Cap ="raju is Rastogi"
+
+print(Cap.capitalize())
+
+# 2 upper() This method returns a string with lowerCase characters replaced by corresponding characters ...
+
+Upper = "the word is not a simple to understand "
+print(Upper.upper())
+
+# 3 lower() --> This method results in a string with upperCase characters replaced by corresponding lowercaser characters..
+
+Lower = " YOU KNOW YOUR SELF BETTER THAN ANYONE THEN DO WHAT YOU WANT TO DO IN OYOUR "
+print(Lower.lower())
+
+# 4 title() ---> This methods result in a string with the first character of each word converts to uppercase ..
+
+Title = " hey man are you strong enough to code , ooh no baby "
+
+print(Title.title())
+
+# 5 find()   --->  This methods find the first occurence of a substring in another string .
+
+Find = " you are a good man "
+
+print(Find.find('o'))
+
+# 6 index()  ---> This methods is similar to find() throws a Value Error if the substring is not found
+
+Index = " hey go "
+
+print(Index.index('go'))
+
+# 7 count()  -----> This methods returns the number of occurences of a substring in given stirng 
+
+Count = " jkldfjdlfkjlkjfkljdfklfjakljdfadfhkhkljf"
+
+print(Count.count('j'))
+
+# 8 isalpha() ----> This method returns true if all the characters in a string are alphabetic a-z or A-Z ,otherwise return false ...
+
+Alpha = "RJOJJG11233"
+
+print(Alpha.isalpha())
+
+# 9 isdigit()  ------> This method returns true if all characters in a string are digits (0-9) , if note return false .
+
+n=  "452453743d4"
+
+# 10,11 islower() and isupper() ----> These methods are similar to the above ,guess or google 
